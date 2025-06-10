5a85e69dcdf2a2a3c949b2ead6c48a147216780c
Zhaosong Zhu <zhaosong.daniel.zhu@gmail.com>
2025-06-10 16:53:54 -0500

    Add Spending and Transaction Page Skeleton (#7)
    * transaction detail card
    
    * transaction page with mock data
    
    * add spending page basic
    
    * add spending page
    
    * add code to support dark and bright mode switch

---
+++ src/atoms/TransactionDetail.tsx
@@ -0,0 +1,38 @@
+import styles from "./TransactionDetail.module.css";
+
+interface TransactionDetailProps {
+  icon: React.ReactNode;
+  merchant: string;
+  date: string;
+  amount: number;
+  type: string; //TODO: update as an enum
+}
+
+const TransactionDetail = ({
+  icon,
+  merchant,
+  date,
+  amount,
+  type,
+}: TransactionDetailProps) => {
+  return (
+    <div className={styles.TransactionDetailContainer}>
+      <div className={styles.LeftContainer}>
+        <div>{icon}</div>
+        <div className={styles.TextContainer}>
+          <div>{merchant}</div>
+          <div>{type}</div>
+        </div>
+      </div>
+
+      <div>
+        {amount.toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TransactionDetail;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Viewport from './Viewport.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode, useMemo } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Viewport from "./Viewport.tsx";
+import { BrowserRouter } from "react-router-dom";
+import {
+  ThemeProvider,
+  createTheme,
+  useMediaQuery,
+  CssBaseline,
+} from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+// Component to provide the theme based on user's system preference
+const AppWithTheme = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstarts an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <BrowserRouter>
+        <Viewport />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Viewport />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <AppWithTheme />
+  </StrictMode>
+);
